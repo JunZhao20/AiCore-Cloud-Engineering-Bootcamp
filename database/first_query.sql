cfc5b1e3cca27957550fd94ffddb1ed47314bcb3
Jun Zhao <junzhao1111@gmail.com>
2023-12-28 12:23:16 +0000

    Add where practical

--- database/first_query.sql
+++ database/first_query.sql
@@ -1,67 +1,115 @@
-SELECT payment_id,
-    customer_id,
-    staff_id,
-    rental_id,
-    amount,
-    payment_date
+-- SELECT payment_id,
+--     customer_id,
+--     staff_id,
+--     rental_id,
+--     amount,
+--     payment_date
 
-FROM payment;
+-- FROM payment;
 
-SELECT * FROM payment
+-- SELECT * FROM payment
 
-SELECT customer_id, amount FROM payment
+-- SELECT customer_id, amount FROM payment
 
+-- SELECT title,
+--            (rental_rate / rental_duration) AS rental_rate_per_day
+-- FROM
+--     film
+
+-- SELECT customer_id,
+--     rental_date
+-- FROM 
+--     rental
+-- ORDER BY
+--     customer_id, rental_date DESC
+
+-- SELECT customer_id,
+--     rental_date
+-- FROM
+--     rental
+-- ORDER BY
+--     customer_id, rental_date DESC
+-- LIMIT
+--     5 OFFSET 40;
+
+-- SELECT title, 
+--        rental_rate,
+--        ROUND((rental_rate / rental_duration), 2) AS rental_rate_per_day, 
+--        (rental_rate * rental_duration) AS total_rental_cost, 
+--        (rental_rate * rental_duration) + replacement_cost AS total_replacement_cost
+-- FROM 
+--     film
+-- WHERE
+--      rental_rate > 2.49
+
+-- SELECT first_name
+-- FROM
+--     actor
+-- WHERE
+--     first_name LIKE 'A%';
+
+-- SELECT address,
+--        district,
+--        phone
+-- FROM
+--     address
+-- WHERE 
+--     district LIKE 'A%'
+--     OR district LIKE 'California%'
+--     OR district LIKE 'C%';
+
+-- SELECT address,
+--        district,
+--        phone
+-- FROM
+--     address
+-- WHERE 
+--     district IN ('Alberta', 'California', 'Hamilton');
+
+
+-- 10 films who's lengths are under 120 mins
 SELECT title,
-           (rental_rate / rental_duration) AS rental_rate_per_day
+    length
 FROM
     film
+WHERE
+    length < 120
 
-SELECT customer_id,
-    rental_date
+
+-- 10 longest films who's rating are G
+SELECT title,
+    length,
+    rating
 FROM 
-    rental
+    film
+WHERE
+    rating = 'G'
 ORDER BY
-    customer_id, rental_date DESC
+    length DESC
 
-SELECT customer_id,
-    rental_date
-FROM
-    rental
-ORDER BY
-    customer_id, rental_date DESC
 LIMIT
-    5 OFFSET 40;
+    10;
 
-SELECT title, 
-       rental_rate,
-       ROUND((rental_rate / rental_duration), 2) AS rental_rate_per_day, 
-       (rental_rate * rental_duration) AS total_rental_cost, 
-       (rental_rate * rental_duration) + replacement_cost AS total_replacement_cost
-FROM 
+-- all transactions where payment has been above $10
+SELECT amount
+FROM
+    payment
+WHERE
+    amount > 10
+
+-- the (replacement) cost per minute of every movie
+SELECT title,
+    ROUND((replacement_cost/length), 2) AS cost_per_minute_of_every_movie
+FROM
     film
-WHERE
-     rental_rate > 2.49
 
-SELECT first_name
+-- top 10 most expensive films to rent, based on the rental rate per hour of the movie
+SELECT title,
+   rental_rate,
+    (length / 60)*rental_rate AS hour_rate
 FROM
-    actor
-WHERE
-    first_name LIKE 'A%';
-
-SELECT address,
-       district,
-       phone
-FROM
-    address
-WHERE 
-    district LIKE 'A%'
-    OR district LIKE 'California%'
-    OR district LIKE 'C%';
-
-SELECT address,
-       district,
-       phone
-FROM
-    address
-WHERE 
-    district IN ('Alberta', 'California', 'Hamilton');
+    film
+ORDER BY
+    hour_rate DESC
+LIMIT
+    10
