ea455b72852791c5100a5a616b4aed6ac9f0c1d1
Jun Zhao <junzhao1111@gmail.com>
2023-12-29 11:21:50 +0000

    Add alter table practice code and practical

--- database/first_query.sql
+++ database/first_query.sql
@@ -113,3 +113,88 @@ ORDER BY
     hour_rate DESC
 LIMIT
     10
+
+SELECT * FROM payment WHERE INTERVAL YEAR payments_this_year = '2007'
+
+DROP TABLE payments_this_year
+
+CREATE TABLE payments_this_year AS(
+    SELECT payment_id,
+        customer_id,
+            staff_id,
+            rental_id,
+            amount,
+            payment_date
+
+    FROM
+        payment
+    WHERE
+        payment_date BETWEEN '2007-01-01 00:00:00' AND '2007-12-31 23:59:59'
+)
+
+SELECT * FROM payments_this_year
+
+ALTER TABLE payments_this_year
+    ALTER COLUMN payment_date TYPE TIMESTAMP(0);
+
+SELECT * FROM category
+
+INSERT INTO category
+    (name, last_update)
+VALUES 
+    ('Thriller', NOW());
+
+CREATE TABLE b_films (
+    title VARCHAR(300) UNIQUE NOT NULL,
+    description TEXT NOT NULL,
+    release_year INTEGER NOT NULL,
+    rating CHAR(5) NOT NULL
+)
+
+INSERT INTO b_films(title, description, release_year, rating)(
+    SELECT title,
+        description,
+        release_year,
+        rating
+    FROM 
+        film
+    WHERE
+        title LIKE 'A%'
+
+)
+SELECT * FROM b_films
+
+SELECT title, rental_rate, release_year
+FROM film
+WHERE title = 'AIRPLANE SIERRA';
+
+UPDATE film
+    SET rental_rate = 0.99,
+        release_year = 2010
+WHERE 
+    title = 'AIRPLANE SIERRA';
+
+ALTER TABLE payments_this_year
+    DROP COLUMN payment_id 
+
+SELECT staff_id, amount, customer_id 
+FROM payments_this_year 
+WHERE amount = 3.99 AND customer_id IN (87, 137);
+
+SELECT amount FROM payments_this_year WHERE payment_date::DATE >= DATE '2007-03-22'
+
+
+UPDATE payments_this_year
+    SET staff_id = 1
+    WHERE
+        amount = 3.99 
+        AND 
+        customer_id IN (87, 137);
+
+UPDATE payments_this_year
+    SET amount = amount + 0.50
+    WHERE
+        payment_date::DATE >= DATE '2007-03-22'
+
+ALTER TABLE payments_this_year
+    RENAME amount TO total_payment_taken
